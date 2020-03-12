32a629d041f6a8dbce557c376ac7a9bc7d05288e
Mark Schmieg <ms147@hw.ac.uk>
2020-03-12 10:43:35 +0000

    added gup to sunburst but mouseover on doesn't work anymore

--- public/js/pieChart.js
+++ public/js/pieChart.js
@@ -1,8 +1,8 @@
 function pie(domEle) {
-
+    
     var pieChartObj = {};
     var data;
-    var margin = { top: 100, right: 160, bottom: 35, left: 30 };
+    var margin = { top: 20, right: 160, bottom: 35, left: 100 };
     var width = 300;
     var height = 300;
 
@@ -11,7 +11,7 @@ function pie(domEle) {
 
     //create the radius of the pie
     var radius = Math.min(width, height) / 2
-    margin
+    
 
     //creates the slices 
     var path = d3.arc()
@@ -37,7 +37,7 @@ function pie(domEle) {
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            .attr("transform", "translate(" + radius + "," + radius + ")");
 
 
         //putting the data into slices
@@ -138,5 +138,75 @@ function pie(domEle) {
     }
 
     return pieChartObj;
+
+}
+
+
+
+function pie2() {
+
+    var data = [
+        { "name": "Scotland", "code": 44 },
+        { "name": "Germany", "code": 49 },
+        { "name": "HK", "code": 85 },
+        { "name": "Spain", "code": 34 },
+
+    ];
+    var margin = { top: 20, right: 160, bottom: 35, left: 100 };
+    var width = 450;
+    var height = 450;
+
+    //create the radius of the pie
+    var radius = Math.min(width, height) / 2
+    margin
+    //link the svg to #pieChart
+    var svg = d3.select('#test')
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + radius + "," + radius + ")");
+
+    //pie chart colours
+    var colours = d3.scaleOrdinal(d3.schemeCategory10);
+
+    //putting the data into slices
+    var pieChart = d3.pie().value(function (d) {
+        return d.code;
+    });
+
+    //creates the slices 
+    var path = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    //giving colour to the different slices and makes the pie chart
+    svg.selectAll("slice")
+        .data(pieChart(data))
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("data-legend", function (d) { return d.data.name; })
+        .attr("data-legend-pos", function (d, i) { return i; })
+        .attr("fill", function (d) {
+            return colours(d.data.code);
+        });
+
+
+    //adding text to the slices in the middle of the slices
+    svg.selectAll("slice")
+        .data(pieChart(data))
+        .enter()
+        .append("text")
+        .attr("transform", function (d) {
+            return "translate(" + path.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function (d) {
+            return d.data.name + " code: " + d.data.code;
+        });
+
+
+
 
 }
---
+++ public/js/sunburst.js
@@ -0,0 +1,195 @@
+
+function sunburst(domEle) {
+
+
+    var sunburstObj = {};
+
+
+    var width = 960;
+    var height = 700;
+    var radius = Math.min(width, height) / 2;
+    var color = d3.scaleOrdinal(d3.schemeCategory20)
+    var data;
+    var root;
+    let burst = d3.select("#" + domEle)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');;
+
+
+   
+
+    sunburstObj.loadAndRenderDataset = function (dataset) {
+
+        data = dataset;    
+        dataProcessor();
+        GUP();
+        return sunburstObj;
+    } 
+
+ 
+
+    var xScale = d3.scaleLinear()
+        .domain([0, radius])
+        .range([0, Math.PI * 2])
+
+    //calculates the sizes of the different data slices
+    var slices = d3.arc()
+        .startAngle(d => xScale(d.x0))
+        .endAngle(d => xScale(d.x1))
+        .innerRadius(d => d.y0)
+        .outerRadius(d => d.y1)
+
+    
+
+   
+
+    function dataProcessor() {
+        //stores the data in d3 hierarchical format for later processing
+        root = d3.hierarchy(data)
+            .sum(function (d) { return d.size });
+
+        //helper to put data in sunburst format
+        var sunburstFormat = d3.partition()
+            .size([360, radius])
+
+
+        // put data into sunburst mode
+        sunburstFormat(root)
+    }
+
+    //tooltip div
+    var tooltip = d3.select("#" + domEle)
+        .append('div')
+        .attr('class', 'label')
+        .style("background", "lightblue")
+        .style("position", "absolute")
+        .style("visibility", "hidden");
+
+    //mouseOver for tooltip
+    function mouseOver() {
+        tooltip.transition()
+            .duration(500)
+            .style("visibility", "visible");
+    }
+
+    //mouseOut for tooltip
+    function mouseOut() {
+        tooltip.transition()
+            .duration(500)
+            .style("visibility", "hidden");
+    }
+
+    function GUP() {
+
+          
+        d3.selectAll("text").remove();
+        
+        var selection = burst.selectAll('path')
+            .data(root.descendants()) //array of all the nodes
+
+
+
+        //displays the data
+        var enterSelection = selection
+            .enter().append('path')
+            .transition()
+            .duration(1000)
+            .delay(1000)
+                .attr("display", function (d) { return d.depth ? null : "none"; })
+                .attr("d", slices)   
+                .style('stroke', '#fff')
+                .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); })
+
+
+
+        //adds text to sunburst
+        burst.selectAll("slice")  
+            .data(root.descendants())
+            .enter()
+            .append("text")
+            .transition()
+            .duration(1000)
+            .delay(1000)
+                .attr("transform", function (d) {
+                    return "translate(" + slices.centroid(d) + ")";
+                })
+                .attr("text-anchor", "middle")
+                .text(function (d) {
+                    return d.data.name;
+                });
+
+
+        //displays the data
+        var updateSelection = selection
+            .transition()
+            .duration(1000)
+            .delay(1000)
+                .attr("display", function (d) { return d.depth ? null : "none"; })
+                .attr("d", slices)
+                .style('stroke', '#fff')
+                .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); })
+
+
+
+        //adds text to sunburst
+        burst.selectAll("slice")
+            .transition()
+            .duration(1000)
+            .delay(1000)
+                .attr("transform", function (d) {
+                    return "translate(" + slices.centroid(d) + ")";
+                })
+                .attr("text-anchor", "middle")
+                .text(function (d) {
+                    return d.data.name;
+                });
+
+        var exitSelection = selection.exit()
+            .classed("exitSelection", true)
+            .transition()
+            .duration(500)
+            .remove();
+
+
+        
+        /*
+        //displays the data
+        var path = burst.selectAll('path')
+            .data(root.descendants()) //array of all the nodes
+            .enter().append('path')
+            .attr("display", function (d) { return d.depth ? null : "none"; })
+            .attr("d", slices)
+            .on("mouseover", mouseOver)
+            .on("mousemove", function (d) {
+                tooltip.html(d.data.size)
+                    .style("left", (d3.event.pageX - 34) + "px")
+                    .style("top", (d3.event.pageY - 12) + "px");
+            })
+            .on("mouseout", mouseOut)
+            .style('stroke', '#fff')
+            .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); })
+
+
+
+        //adds text to sunburst
+        burst.selectAll("slice")
+            .data(root.descendants())
+            .enter()
+            .append("text")
+            .attr("transform", function (d) {
+                return "translate(" + slices.centroid(d) + ")";
+            })
+            .attr("text-anchor", "middle")
+            .text(function (d) {
+                return d.data.name;
+            });
+            */
+        return sunburstObj;
+    }
+
+    return sunburstObj;
+
+}
