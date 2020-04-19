5d3d5a2a71d007373a8a730c43c279c8edb8c65c
cmhayden <haydencallum4@gmail.com>
2020-04-19 18:50:25 +0100

    tidy packLayout.js

--- public/js/packLayout.js
+++ public/js/packLayout.js
@@ -1,140 +1,5 @@
-// function packLayout(domEle)
-// {
-
-//     var packLayoutObj = {};
-
-//     var width = 700;
-//     var height = 800;
-//     var root;
-//     var data;
-//     var margin = { top: 20, right: 160, bottom: 35, left: 100 };
-//     var color = d3.scaleOrdinal(d3.schemeCategory20)
-//     var previousNode;
-//     var backArr = [];
-
-//     packLayoutObj.loadAndRenderDataset = function (dataset) {
-
-//         data = dataset;
-       
-//         render(data);
-//         return packLayoutObj;
-//     } 
-
-   
-
-
-//     var pack = d3.select("#" + domEle)
-//         .append('svg')
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append('g')
-       
-        
-
-
-//     function render(data) {
-//         dataProcessor(data);
-//         draw();
-//     }
-
-
-//     function dataProcessor(dataSet) {
-//         //stores the data in d3 hierarchical format for later processing
-//         root = d3.hierarchy(dataSet)
-//             .sum(function (d) { return d.size });
-
-//         //helper to put data in pack format
-//         var packFormat = d3.pack().size([width, height]);
-
-
-//         // put data into pack mode
-//         packFormat(root)
-//         packFormat.padding(10)
-
-        
-//         //sets previous node to root to start
-//         previousNode = root
-//     }
-
-
-//     //click function
-//     function clicked(d) {
-
-//         //checking to see if the parent circle pressed has the same name as the previous item clicked, if so then it means that the parent circle was clicked 
-//         if (previousNode.data["name"].localeCompare(d.data["name"]) == 0) {
-
-
-//             render(backArr[backArr.length - 1]);
-//             data = backArr[backArr.length - 1];
-//             backArr.splice(backArr.length - 1, 1);
-
-
-//         } else {
-
-//             backArr.push(data);
-//             render(d.data);
-//             data = d.data;
-//             previousNode = d;
-
-//         }
-
-
-//     }
-
-//     function draw() {
-
-//         d3.selectAll("text").remove();
-//         d3.selectAll("g").selectAll("circle").remove();
-
-//         var selection = pack.selectAll('circle')
-//             .data(root.descendants()) //array of all the nodes
-
-//         console.log(root.descendants())
-
-//         var enterSelection = selection
-//             .enter().append('circle')                   
-//             .attr('cx', function (d) { return d.x; })
-//             .attr('cy', function (d) { return d.y; })
-//             .attr('r', function (d) { return d.r; })          
-//             .style("fill", function (d) { return color(d.data.name); })
-//             .on('click', clicked)
-
-//         enterSelection.append("text")
-//             .attr("dx", function(d){return +200})
-//             .text(function(d){
-//                 console.log(d.data.name)
-//                 return d.data.name
-//             })
-       
-//         pack.selectAll('circle')
-//             .data(root.descendants())
-//             .append('text')
-//             .attr('y', function(d){ return d.r*.25; })
-//             .attr('class', 'packLayoutLabel')
-//             .style('font-size', function(d){ return d.r*.4})            
-//             .text(function (d) {
-//                 console.log(d.data.name);
-//                 return d.data.name;
-//             })       
-              
-
-//         return packLayoutObj;
-//     }
-
-//     return packLayoutObj;
-// }
-
-
-
-
-/* 
- * With everything grouped. All comented out
- * (should be uncommentable with ctrl+forwad slash when highlighted to make life easy)
- * CMHayden
- */
 function packLayout(domEle, tree, sunburst)
 {
-
     var packLayoutObj = {};
     var tree;
     var sunburst;
@@ -150,25 +15,27 @@ function packLayout(domEle, tree, sunburst)
     packLayoutObj.loadAndRenderDataset = function (dataset, bool, d, removeBool, removeFromOut) {
 
         if (d) {
-                if (removeFromOut) {
-                    if(removeBool) {
-                        data = backArr[backArr.length - 1];
-                        backArr.splice(backArr.length - 1, 1);  
-                    } else {
-                        backArr.push(data);
-                        data = d.data;
-                        previousNode = d;
-                    }
+            if (removeFromOut) {
+                if(removeBool) {
+                    data = backArr[backArr.length - 1];
+                    backArr.splice(backArr.length - 1, 1);  
+                } else {
+                    backArr.push(data);
+                    data = d.data;
+                    previousNode = d;
                 }
             }
+        }
 
         data = dataset;
        
         render(data);
+
         if(bool) {
             tree.loadAndRenderDataset(data, false, d, removeBool, true);
             sunburst.loadAndRenderDataset(data, false, d, removeBool, true);
         }
+
         return packLayoutObj;
     } 
 
@@ -177,20 +44,15 @@ function packLayout(domEle, tree, sunburst)
         sunburst = sun;
     } 
 
-
     var pack = d3.select("#" + domEle)
         .append('svg')
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-       
-        
-
 
     function render(data) {
         dataProcessor(data);
         draw();
     }
-
 
     function dataProcessor(dataSet) {
         //stores the data in d3 hierarchical format for later processing
@@ -200,33 +62,32 @@ function packLayout(domEle, tree, sunburst)
         //helper to put data in pack format
         var packFormat = d3.pack().size([width, height]);
 
-
         // put data into pack mode
         packFormat(root)
         packFormat.padding(10)
-
-        
+       
         //sets previous node to root to start
         previousNode = root
     }
-
 
     //click function
     function clicked(d) {
         if (d.data["name"] != "Countries") {
             //checking to see if the parent circle pressed has the same name as the previous item clicked, if so then it means that the parent circle was clicked 
             if (previousNode.data["name"].localeCompare(d.data["name"]) == 0) {
-                console.log("true");
+
                 packLayoutObj.loadAndRenderDataset(backArr[backArr.length - 1], true, d, true, false);
                 data = backArr[backArr.length - 1];
                 backArr.splice(backArr.length - 1, 1);
+
             } else {
                 if (typeof d.data.valid == "undefined") {
-                    console.log("false");
+
                     backArr.push(data);
                     packLayoutObj.loadAndRenderDataset(d.data, true, d, false, false);
                     data = d.data;
                     previousNode = d;
+
                 } else {
                     console.log("stop");
                 }
@@ -255,7 +116,6 @@ function packLayout(domEle, tree, sunburst)
             .style("visibility", "hidden");
     }
 
-
     function draw() {
 
         d3.select("#" + domEle).selectAll("text").remove();
@@ -264,7 +124,6 @@ function packLayout(domEle, tree, sunburst)
         var selection = pack.selectAll('circle')
             .data(root.descendants()) //array of all the nodes
       
-
         var enterSelection = selection
             .enter().append('g')
             
@@ -282,15 +141,10 @@ function packLayout(domEle, tree, sunburst)
                     .style("left", (d3.event.pageX - 100) + "px")
                     .style("top", (d3.event.pageY - 50) + "px");
             })
-            .on("mouseout", mouseOut)
-            
-              
+            .on("mouseout", mouseOut)      
 
         return packLayoutObj;
     }
 
     return packLayoutObj;
 }
-
-
-
